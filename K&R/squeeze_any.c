2ec8ff73de9c323e67f8fabe476c3669b3e76849
mofink <mosheafink@gmail.com>
2019-02-28 15:20:34 -0500

    Update and rename squeeze.c to squeeze_any.c

--- K&R/squeeze_any.c
+++ K&R/squeeze_any.c
@@ -1,15 +1,20 @@
-/*print s1 removing all characters found in s2 */
+/*squeeze prints s1 removing all characters found in s2 */
+/* any returns index of first appearance of something in s2 found in s1 */
+
 
 #include <stdio.h>
 
 void squeeze(char s1[], char s2[]);
 int is_in(char c, char s[]);
+int any(char s1[], char s2[]);
 
 int main()
 {
 	char s1[] = "hello my name is Bruce";
 	char s2[] = "lmn";
 	squeeze(s1,s2);
+	char s3[] = "hello my name is Bruce"; /* reset for test case */
+	printf("%d\n", any(s3,s2));
 	return 0;
 }
 
@@ -21,9 +26,21 @@ void squeeze(char s1[], char s2[])
 		if (is_in(s1[i],s2) == 0)
 			s1[j++] = s1[i];
 	}
-	s1[j++] = '\n';
-	printf("%s",s1);
+	s1[j++] = '\0';
+	printf("%s\n",s1);
 }
+
+int any(char s1[], char s2[])
+{
+	int i;
+	for (i = 0; s1[i] != '\0'; ++i)
+	{
+		if (is_in(s1[i], s2) == 1)
+			return i; 
+	}
+	return -1;
+}
+
 
 int is_in(char c, char s[])
 {
