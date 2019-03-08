afc8a34f5ee5e0f8aef4585240fa2fe02828f488
mofink <mosheafink@gmail.com>
2019-03-08 14:10:41 -0500

    Update playing_with_time.c

--- kernel_programming/playing_with_time.c
+++ kernel_programming/playing_with_time.c
@@ -5,13 +5,13 @@
 
 int main(void)
 {
-	time_t *now; /* now is a pointer to time_t */
-	time(now); /* returns time in seconds to time_t now */
+	time_t now = time(NULL); /* now is time_t 
+				  				returns time in seconds to time_t now */
 
-	printf("The current time is %s\n",ctime(now));
+	printf("The current time is %s\n",ctime(&now));
 
 
-	/* We can also call time() another way */
+
 	time_t also_now = time(NULL); /* also_now is time_t */
 	struct tm *time_struct;
 	time_struct = localtime(&also_now);
