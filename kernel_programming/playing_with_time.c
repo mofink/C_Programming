c050bde0694a7771d8ed20060c40ad51a42130fe
mofink <mosheafink@gmail.com>
2019-03-08 12:51:57 -0500

    Create playing_with_time.c

---
+++ kernel_programming/playing_with_time.c
@@ -0,0 +1,22 @@
+/* demonstrates that ctime() is the same as asctime(localtime()) */
+
+#include <stdio.h>
+#include <time.h>
+
+int main(void)
+{
+	time_t *now; /* now is a pointer to time_t */
+	time(now); /* returns time in seconds to time_t now */
+
+	printf("The current time is %s\n",ctime(now));
+
+
+	/* We can also call time() another way */
+	time_t also_now = time(NULL); /* also_now is time_t */
+	struct tm *time_struct;
+	time_struct = localtime(&also_now);
+	printf("The curent time is %s\n",asctime(time_struct));
+
+
+	return 0;
+}
