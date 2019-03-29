5c6cf1cb6aa0e63abe645d64500ad7965ec3d38b
mofink <mosheafink@gmail.com>
2019-03-29 15:35:21 -0400

    working on some errors for reading in arguments

--- kernel_programming/shell_program.c
+++ kernel_programming/shell_program.c
@@ -1,5 +1,3 @@
-/* NOTE: THIS WAS WRITTEN FOR LINUX SYSTEMS
-   it should run on macOS but the compiler might grumble a bit */
 
 #include <stdio.h>
 #include <string.h>
@@ -20,13 +18,14 @@ struct command_struct /*read the cmd line input into here */
 int getword(char *s);
 void assign_to_struct(struct command_struct *command,char * s, int j);
 void copy_word(char *s1, char *s2);
+int count_array(char **s);
 int buildpath(char *s,char *mypath, char *dir);
 
 int main(void)
 {	
 	printf("Welcome to Mo's Terminal Application\nTo quit use the 'exit' command\n");
 	int exit_flag = 0;
-	char *buf = (char *) malloc(sizeof(char *));
+	char *buf;
 	buf = getlogin();
 	char cwd[MAXLEN];
 	while (!exit_flag)
@@ -44,43 +43,69 @@ int main(void)
 		char *dir = ".";
 		
 		printf("%s@%s$ ", buf, getcwd(cwd, sizeof(cwd)));
+		int *num_args = (int *) malloc(sizeof(int)); //make num_args global
 
 		for (;;) /*infinite loop waiting for input */
 		{	
 			char word[MAXLEN]; /*have to redefine buffer each iter because of pointer assignment */
 			c = getword(word);
 			assign_to_struct(command,word,++j);
-
 			if (c == '\n')
+				assign_to_struct(command,NULL,++j);
+				*num_args = count_array(command->argv);
+				printf("%d\n",*num_args);
 				break;
 		}
 		
 
 		char *filename = (char *) malloc(sizeof(char)*MAXLEN);
 
-		char *mypath=getenv("PATH"); /* cheating */
-		if (strcmp(command->name,"exit")) /*skip buildpath if cmd = exit. need to generalize this */
+		char *mypath=getenv("PATH"); // cheating 
+		if (strcmp(command->name,"exit")) // skip buildpath if cmd = exit
 		{
 			if (buildpath(filename,mypath,dir))
-			{
+			{	
+
+				int i;
+				for (i = 0; i < *num_args; ++i)
+				{
+					printf("%s ",command->argv[i]);
+				}
+
 				printf("The complete file path for %s is %s\n",command->name,filename);
 				pid_t pid;
 				if ((pid = fork()) < 0)
 				{
-					printf("Failed to fork.\n");
+					printf("Failed to fork. Aborting...\n");
+					abort();
 				}
-				else if (pid == 0) /* This is the child process */
+				else if (pid == 0) // This is the child process 
 				{
-					printf("Hello from the child process: calling 'execv' to execute %s\n",filename);
+
+					if (*num_args < 0)
+					{
+						printf("Hello from the child process: calling 'execvp' to execute %s with no arguments\n...\n",filename);
+					}
+					else
+					{
+						int i;
+						printf("Hello from the child process: calling 'execvp' to execute %s with arguments: ",filename);
+						for (i = 0; i < *num_args; ++i)
+						{
+							printf("%s ",command->argv[i]);
+						}
+						printf("\n");
+					}
 					exit(0);
 				}
 				else
 				{
-					printf("Hello from the parent process with pid %d: waiting for child to terminate\n",pid);
+					printf("Hello from the parent process with pid %d: waiting for child to terminate\n...\n",pid);
 					int status;
     				waitpid(pid, &status, 0);
     				printf("Child process returned with exit status %d\n",status);
 				}	
+				
 			}
 		}
 
@@ -95,6 +120,8 @@ int main(void)
 		
 		{ /* local scope */
 			int i = 0;
+			*num_args = 0;
+			free(command->name);
 			while(command->argv[i++])
 			{
 				free(command->argv[i]);
@@ -127,14 +154,31 @@ void assign_to_struct(struct command_struct *command, char * s, int j)
 		copy_word(command->name, s);
 	}
 	else
-	{	
-		copy_word(command->argv[j-2], s);
+	{
+		if (s)
+		{	
+			copy_word(command->argv[j-2], s);
+		}
+		else //s is NULL
+		{
+			command->argv[j-2] = NULL;
+		}
 	}
 }
 
 void copy_word(char *s1, char *s2)
 {
-	while((*s1++ = *s2++)); /* could use strcpy but I love seeing this on paper */
+	while((*s1++ = *s2++)); // could use strcpy but I love seeing this on paper 
+}
+
+int count_array(char **s)
+{
+	int i = 0;
+	while(command->argv[i])
+	{
+		i++;
+	}
+	return i-1; //account for NULL appended to the end of argument list
 }
 
 int buildpath(char *filename, char *mypath, char *dir)
